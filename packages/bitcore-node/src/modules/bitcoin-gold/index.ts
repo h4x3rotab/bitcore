81abad34ed8c7a91753396786f8ded2b04b16ee2
h4x3rotab <h4x3rotab@gmail.com>
2019-10-28 17:24:07 +0000

    Correct btg library: bitcore-lib-btg

--- packages/bitcore-node/src/modules/bitcoin-gold/index.ts
+++ packages/bitcore-node/src/modules/bitcoin-gold/index.ts
@@ -5,7 +5,7 @@ import { BTGStateProvider } from '../../providers/chain-state/btg/btg';
 export default class BCHModule extends BaseModule {
   constructor(services) {
     super(services);
-    services.Libs.register('BTG', 'bitcore-lib-gold', 'bitcore-p2p');
+    services.Libs.register('BTG', 'bitcore-lib-btg', 'bitcore-p2p');
     services.P2P.register('BTG', BitcoinP2PWorker);
     services.CSP.registerService('BTG', new BTGStateProvider());
   }
